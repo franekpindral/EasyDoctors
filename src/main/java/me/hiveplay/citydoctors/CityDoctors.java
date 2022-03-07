9e9ad38cd13593bd50a1b40ec8c2828aa39399b5
hiveplay <franek.pindral09@gmail.com>
2022-03-07 18:14:10 +0100

    Creating project

---
+++ src/main/java/me/hiveplay/citydoctors/CityDoctors.java
@@ -0,0 +1,18 @@
+package me.hiveplay.citydoctors;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CityDoctors extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Hejo");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
